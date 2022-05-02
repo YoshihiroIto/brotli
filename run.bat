c0aca44cf8c340073bfc1b3da8aa2478863a6589
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-05-03 08:17:55 +0900

    First commit

---
+++ run.bat
@@ -0,0 +1,4 @@
+cd /d %~dp0
+
+call node %~dp0\build\main.js %1
+
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import { brotliCompressSync } from 'zlib';
+import process from 'process';
+
+if (process.argv.length != 3) {
+  console.log('error');
+  process.exit(1);
+}
+
+const inputFile = process.argv[2];
+
+const input = fs.readFileSync(inputFile);
+
+const compressed = brotliCompressSync(input);
+
+fs.writeFile(inputFile + '.brotli', compressed, () => {});
